f6d0faaa11ff0efe8ddee88abaa57586b0fc8bd6
Gruit <gurit.nat@bk.ru>
2022-01-04 18:12:23 +0300

    app part 1 true

--- sctipt.js
+++ sctipt.js
@@ -1,17 +1,22 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели'); //можно было добавить унарный +
 
 let personalMobieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: {},
+    genres: {}, // не объект а массив
     privat: false,
 }
-
-let ques = prompt('Один из последних просмотренных фильмов?');
+// нужно задать по два раза
+let ques = prompt('Один из последних просмотренных фильмов?'); 
 let quesTwo = prompt('На сколько оцените его?');
 
+let q = prompt('Один из последних просмотренных фильмов?'); 
+let qTwo = prompt('На сколько оцените его?');
+
 personalMobieDB.movies[ques] = quesTwo;
+personalMobieDB.movies[q] = qTwo;
+
 
 
 console.log(personalMobieDB);
